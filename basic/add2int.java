1b36e0c93ea3bf89dfa1909cc020f4454f76cee4
varnikasood <varnikasood2004@gmail.com>
2023-10-16 20:17:55 +0530

    added 2nd prog

---
+++ basic/add2int.java
@@ -0,0 +1,11 @@
+public class add2int {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // adding
+        int sum = a + b;
+        System.out.println(a + " + " + b + " = " + sum);
+    }
+}
